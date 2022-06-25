1cd2c0e6cf6821f1dd980adb251565574ef21b1e
Dwerjeson Garcia <79482342+dwerjerson@users.noreply.github.com>
2022-06-25 00:27:37 -0300

    Add files via upload

---
+++ ex004.py
@@ -0,0 +1,3 @@
+# Faça um programa que leia pelo teclado e mostre na tela o seu tipo primitivo, e todas as informaçoes possíveis sobre ele:
+# Se ele é um número, uma letra, se é inteiro, com ponto flutuante... e assim por diante.
+
