602a250d9e30904861e36a836a5389fb730d90a7
Dwerjeson Garcia <79482342+dwerjerson@users.noreply.github.com>
2022-06-26 11:15:16 -0300

    Add files via upload

--- ex004.py
+++ ex004.py
@@ -1,3 +1,10 @@
 # Faça um programa que leia pelo teclado e mostre na tela o seu tipo primitivo, e todas as informaçoes possíveis sobre ele:
 # Se ele é um número, uma letra, se é inteiro, com ponto flutuante... e assim por diante.
 
+variavel = input("Digite algo: ")
+print(f'Essa variável é um letra ou número? {variavel.isalnum()}\n'
+      f'É uma letra? {variavel.isalpha()}\n'
+      f'É um número? {variavel.isdigit()}\n'
+      f'É um caractere especial? {variavel.isascii()}\n'
+      f'É um decimal? {variavel.isdecimal()}\n'
+      f'É um identificador? {variavel.isidentifier()}')
